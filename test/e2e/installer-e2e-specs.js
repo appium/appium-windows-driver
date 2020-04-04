5f4a908312588b0e9291d22748c36fd799c68023
Mykola Mokhnach <mokhnach@gmail.com>
2020-04-04 10:17:10 +0200

    Try with skip

--- test/e2e/installer-e2e-specs.js
+++ test/e2e/installer-e2e-specs.js
@@ -14,14 +14,18 @@ describe('installer', function () {
   });
 
   it('should fail if not admin', async function () {
-    if (!await isAdmin()) {
-      await setupWAD().should.be.rejectedWith(/administrator/);
+    if (await isAdmin()) {
+      return this.skip();
     }
+
+    await setupWAD().should.be.rejectedWith(/administrator/);
   });
 
   it('should setup and validate WinAppDriver as admin', async function () {
-    if (await isAdmin()) {
-      await setupWAD(); // contains its own verification of md5 so not much to do
+    if (!await isAdmin()) {
+      return this.skip();
     }
+
+    await setupWAD(); // contains its own verification of md5 so not much to do
   });
 });
