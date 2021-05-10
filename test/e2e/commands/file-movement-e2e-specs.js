2f23b96d8fd4a168a4a16d3072a9124f7dd96b69
Dmitry Obraztsov <d.obrazcov@gmail.com>
2021-05-10 15:32:48 +0300

    New: Added file movements functionality

---
+++ test/e2e/commands/file-movement-e2e-specs.js
@@ -0,0 +1,82 @@
+import wd from 'wd';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import path from 'path';
+import { tempDir, fs } from 'appium-support';
+import { startServer } from '../../../lib/server';
+
+chai.should();
+chai.use(chaiAsPromised);
+
+const TEST_PORT = 4788;
+const TEST_HOST = 'localhost';
+
+
+describe('file movement', function () {
+  let server;
+  let driver;
+  let remotePath;
+
+  before(async function () {
+    server = await startServer(TEST_PORT, TEST_HOST);
+  });
+
+  after(async function () {
+    if (server) {
+      await server.close();
+    }
+    server = null;
+  });
+
+  beforeEach(async function () {
+    if (server) {
+      driver = wd.promiseChainRemote(TEST_HOST, TEST_PORT);
+      await driver.init({
+        app: 'Microsoft.WindowsCalculator_8wekyb3d8bbwe!App',
+        platformName: 'Windows',
+      });
+    }
+  });
+
+  afterEach(async function () {
+    if (driver) {
+      await driver.quit();
+    }
+    if (remotePath) {
+      if (await fs.exists(remotePath)) {
+        await fs.rimraf(path.dirname(remotePath));
+      }
+    }
+    remotePath = null;
+    driver = null;
+  });
+
+  it('should push and pull a file', async function () {
+    let stringData = `random string data ${Math.random()}`;
+    let base64Data = Buffer.from(stringData).toString('base64');
+    remotePath = await tempDir.path({ prefix: 'appium', suffix: '.tmp' });
+
+    await driver.pushFile(remotePath, base64Data);
+
+    // get the file and its contents, to check
+    let remoteData64 = await driver.pullFile(remotePath);
+    let remoteData = Buffer.from(remoteData64, 'base64').toString();
+    remoteData.should.equal(stringData);
+  });
+
+  it('should be able to delete a file', async function () {
+    const stringData = `random string data ${Math.random()}`;
+    const base64Data = Buffer.from(stringData).toString('base64');
+    remotePath = await tempDir.path({ prefix: 'appium', suffix: '.tmp' });
+
+    await driver.pushFile(remotePath, base64Data);
+
+    let remoteData64 = await driver.pullFile(remotePath);
+    let remoteData = Buffer.from(remoteData64, 'base64').toString();
+    remoteData.should.equal(stringData);
+
+    await driver.execute('windows: deleteFile', { remotePath });
+
+    await driver.pullFile(remotePath).should.eventually.be.rejectedWith(/No such file/);
+  });
+});
