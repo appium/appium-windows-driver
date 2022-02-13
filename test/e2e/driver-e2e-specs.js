079fa4504d87ece8ea753aa6eed28a77bbcf5bd9
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2022-02-13 01:12:04 -0800

    comment out e2e with wd

--- test/e2e/driver-e2e-specs.js
+++ test/e2e/driver-e2e-specs.js
@@ -1,52 +1,52 @@
-import wd from 'wd';
-import chai from 'chai';
-import chaiAsPromised from 'chai-as-promised';
-import { startServer } from '../../lib/server';
-import { isAdmin } from '../../lib/installer';
+// import wd from 'wd';
+// import chai from 'chai';
+// import chaiAsPromised from 'chai-as-promised';
+// import { startServer } from '../../lib/server';
+// import { isAdmin } from '../../lib/installer';
 
-chai.should();
-chai.use(chaiAsPromised);
+// chai.should();
+// chai.use(chaiAsPromised);
 
-const TEST_PORT = 4788;
-const TEST_HOST = 'localhost';
+// const TEST_PORT = 4788;
+// const TEST_HOST = 'localhost';
 
-describe('Driver', async function () {
-  if (!await isAdmin()) {
-    return;
-  }
+// describe('Driver', async function () {
+//   if (!await isAdmin()) {
+//     return;
+//   }
 
-  let server;
-  let driver;
+//   let server;
+//   let driver;
 
-  before(async function () {
-    server = await startServer(TEST_PORT, TEST_HOST);
-  });
+//   before(async function () {
+//     server = await startServer(TEST_PORT, TEST_HOST);
+//   });
 
-  after(async function () {
-    if (server) {
-      await server.close();
-    }
-    server = null;
-  });
+//   after(async function () {
+//     if (server) {
+//       await server.close();
+//     }
+//     server = null;
+//   });
 
-  beforeEach(function () {
-    if (server) {
-      driver = wd.promiseChainRemote(TEST_HOST, TEST_PORT);
-    }
-  });
+//   beforeEach(function () {
+//     if (server) {
+//       driver = wd.promiseChainRemote(TEST_HOST, TEST_PORT);
+//     }
+//   });
 
-  afterEach(async function () {
-    if (driver) {
-      await driver.quit();
-    }
-    driver = null;
-  });
+//   afterEach(async function () {
+//     if (driver) {
+//       await driver.quit();
+//     }
+//     driver = null;
+//   });
 
-  it('should run a basic session using a real client', async function () {
-    await driver.init({
-      app: 'Microsoft.WindowsCalculator_8wekyb3d8bbwe!App',
-      platformName: 'Windows',
-    });
-    await driver.source().should.eventually.be.not.empty;
-  });
-});
+//   it('should run a basic session using a real client', async function () {
+//     await driver.init({
+//       app: 'Microsoft.WindowsCalculator_8wekyb3d8bbwe!App',
+//       platformName: 'Windows',
+//     });
+//     await driver.source().should.eventually.be.not.empty;
+//   });
+// });
